c267677d7be9382d26ba12c6728bf6db736088f4
labarruffe <lucas_barruffe@hotmail.com>
2019-09-11 20:19:35 -0300

    Create Program class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace whatsApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Creating an Whatsapp...");
+            Whatsapp whatsapp = new Whatsapp();
+            
+            Console.WriteLine("Add new contact:");
+            Console.WriteLine("Name ?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Phone number ?");
+            string phone = Console.ReadLine();
+
+            Contact newContact = whatsapp.CreateContact(name, phone); 
+
+            Console.WriteLine("Send as SMS, Audio or Photo ?");
+            string messageType = Console.ReadLine();
+            Console.WriteLine("Write you content:");
+            string content = Console.ReadLine();
+            
+            Message newMessage = whatsapp.SendMessage(newContact, messageType, content);
+
+            Console.WriteLine(newMessage);
         }
     }
 }
